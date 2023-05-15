22569aaa35ea0f3036c64067c0b09efe5e622dac
Kamil Akbarov <akbarov@farmani.ru>
2023-05-15 17:32:22 +0300

    15.05.2023

---
+++ android/app/src/main/kotlin/com/alomat/todo_hive_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.alomat.todo_hive_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/todo.dart
@@ -0,0 +1,20 @@
+import 'package:hive/hive.dart';
+part 'todo.g.dart';
+
+@HiveType(typeId: 0)
+
+class Todo extends HiveObject {
+  @HiveField(0)
+  String id;
+
+  @HiveField(1)
+  bool complete;
+
+  @HiveField(2)
+  String note;
+
+  @HiveField(3)
+  String task;
+
+  Todo({this.complete = false, this.note = '', this.task = '', this.id = '0'});
+}
